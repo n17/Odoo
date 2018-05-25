8dbf9d473899544fbe780fa29914ed6368f9d23c
Oskars Zalitis <o.zalitis@gmail.com>
2018-05-25 16:40:26 +0300

    The partner_company_code module for Odoo 11.

---
+++ __manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "partner_company_code",
+
+    'summary': """
+        Adds a "company_code" field to the "res.partner" data model.""",
+
+    'description': """
+         The purpose of this module is to add a new Char field called "company_code" –
+meant for the company registration identification number – on the "res.partner" data model.
+The field is also added to the customer / partner form.
+    """,
+
+    'author': "Oskars Zālītis",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/partner.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+}
---
+++ models/partner.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models, api, exceptions
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    # Adds a new field to the res.partner model.
+    company_code = fields.Char(string="Company code")
+
+    # Adds a constraint to the company_code field.
+    @api.constrains('company_code')
+    def _check_cc_number(self):
+        for r in self:
+            if self.env['res.partner'].search_count([('company_code','=', r.company_code)]) > 3:
+                raise exceptions.ValidationError("No more than 3 companies may have the same company code!")
+
